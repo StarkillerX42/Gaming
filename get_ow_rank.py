5368acd35a1532b227e9f743bfa1113149bee3ea
StarkillerX42 <dgatlin5@gmail.com>
2018-11-17 13:41:08 -0700

    init

---
+++ get_ow_rank.py
@@ -0,0 +1,30 @@
+"""This function grabs my rank and saves it to a file"""
+import requests
+import datetime
+import os
+now = datetime.datetime.now()
+print("Get OW Rank started at {}".format(now))
+
+data = requests.get("https://ow-api.com/v1/stats/pc/us/Starkiller42-11691/profile")
+# print(data.json())
+try:
+    rank = int(data.json()["rating"])
+except:
+    rank = 0
+
+rankpath = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), "ow_ranks.txt")
+# print(rankpath)
+with open(rankpath, "r") as fil:
+    lines = fil.readlines()
+    if str(now.date()) not in lines[-1]:
+        needrank = True
+    else:
+        needrank = False
+if needrank:
+    with open(rankpath, "a") as fil:
+        print("    Writing new SR")
+        fil.write("{}: {}\n".format(now.date(), rank))
+else:
+    print("    SR already written today")
+
---
+++ plot_ow_ranks.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy.time import Time
+dates = []
+ranks = []
+with open("ow_ranks.txt", "r") as rank_fil:
+    for i, line in enumerate(rank_fil):
+        date, rank = line.split(":")
+        dates.append(Time(date).plot_date)
+        ranks.append(int(rank))
+dates = np.array(dates)
+ranks = np.array(ranks)
+good_values = ranks != 0
+fig = plt.figure(figsize=(10, 6))
+ax = fig.gca()
+ax.plot_date(dates[good_values], ranks[good_values],"-", tz=None,
+             xdate=True, ydate=False, drawstyle="steps-post")
+ax.set_title("Overwatch Ranks Over Time")
+ax.set_xlabel("Date")
+ax.set_ylabel("Rank")
+ax.axhline(2000, c="gold", linewidth=0.5)
+ax.axhline(2500, c="green", linewidth=2)
+ax.set_ylim(2300, 2700)
+fig.savefig("ranks.png")
