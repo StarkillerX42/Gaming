c9d9f1cd97935d4e8d99c142cd6e81feb16d3fad
StarkillerX42 <dgatlin5@gmail.com>
2019-01-10 16:02:11 -0700

    Started CS:GO archive, PEP8ed the OW stuff. Added some details to the OW plot

--- get_csgo_rank.py
+++ get_csgo_rank.py
@@ -1,18 +1,45 @@
-"""This function grabs my overwatch rank and saves it to a file"""
+"""This function grabs my CS:GO rank and saves it to a file"""
 import requests
 import datetime
 import os
 now = datetime.datetime.now()
-print("Get OW Rank started at {}".format(now))
+print("Get CS:GO Rank started at {}".format(now))
 
 data = requests.get("http://api.steampowered.com/ISteamUserStats/GetUserStatsFo"
                     "rGame/v0002/?appid=730&key=E65AB2D9A4C8C42DCEEBCCD8D56BF02"
                     "9&steamid=76561198199499279")
-dumb_stats = data.json()["playerstats"]["stats"]  # Because CSGO's format sucks
+# Because CSGO's format sucks, I reformat it into my own dictionary
+dumb_stats = data.json()["playerstats"]["stats"]
 stats = {}
 for i, elem in enumerate(dumb_stats):
     stats[elem["name"]] = elem["value"]
 
-print("\n".join(stats.keys()))
-kdr = stats["total_kills"]/stats["total_deaths"]
-kills_per_round = stats["total_kills"]/stats["total_rounds_played"]
+# print("\n".join(stats.keys()))
+try:
+    kdr = stats["total_kills"]/stats["total_deaths"]
+except Exception:
+    kdr = None
+try:
+    kills_per_round = stats["total_kills"]/stats["total_rounds_played"]
+except Exception:
+    kills_per_round = None
+try:
+    rank = stats["skill_group"]
+except Exception:
+    rank = None
+
+archive_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                            "csgo_data.txt")
+with open(archive_path, "r") as fil:
+    lines = fil.readlines()
+    if str(now.date()) not in lines[-1]:
+        need_rank = True
+    else:
+        need_rank = False
+
+if need_rank:
+    with open(archive_path, "a") as fil:
+        print("    Writing new SG")
+        fil.write(f"{now.date()}, {kdr:.3f}, {kills_per_round:.0f}, {rank}\n")
+else:
+    print("    SG already written today")
--- get_ow_rank.py
+++ get_ow_rank.py
@@ -5,26 +5,26 @@ import os
 now = datetime.datetime.now()
 print("Get OW Rank started at {}".format(now))
 
-data = requests.get("https://ow-api.com/v1/stats/pc/us/Starkiller42-11691/profile")
+data = requests.get("https://ow-api.com/v1/stats/pc/us/Starkiller42-11691/"
+                    "profile")
 # print(data.json())
 try:
     rank = int(data.json()["rating"])
-except:
+except Exception:
     rank = 0
 
-rankpath = os.path.join(os.path.dirname(
+rank_path = os.path.join(os.path.dirname(
         os.path.abspath(__file__)), "ow_ranks.txt")
-# print(rankpath)
-with open(rankpath, "r") as fil:
+# print(rank_path)
+with open(rank_path, "r") as fil:
     lines = fil.readlines()
     if str(now.date()) not in lines[-1]:
-        needrank = True
+        need_rank = True
     else:
-        needrank = False
-if needrank:
-    with open(rankpath, "a") as fil:
+        need_rank = False
+if need_rank:
+    with open(rank_path, "a") as fil:
         print("    Writing new SR")
         fil.write("{}: {}\n".format(now.date(), rank))
 else:
     print("    SR already written today")
-
