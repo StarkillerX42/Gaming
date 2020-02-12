564d059e6d640cb0ded1a3a86decb0769cfaed0c
Dylan Gatlin <dgatlin5@gmail.com>
2020-02-12 01:09:14 -0700

    Fixed rolelock bugs with missing data

--- get_ow_rank.py
+++ get_ow_rank.py
@@ -2,38 +2,50 @@
 """This function grabs my overwatch rank and saves it to a file"""
 import requests
 import datetime
-import os
+from pathlib import Path
+import starcoder42 as s
+
 now = datetime.datetime.now()
 print("Get OW Rank started at {}".format(now))
 
-data = requests.get("https://ow-api.com/v1/stats/pc/us/Starkiller42-11691/"
-                    "profile")
-# print(data.json())
-# try:
 pc = {'tank': 0, 'damage': 0, 'support': 0}
-ranks = data.json()["ratings"]
-for rank in ranks:
-    pc[rank['role']] = rank['level']
+try:
+    data = requests.get("https://ow-api.com/v1/stats/pc/us/Starkiller42-11691/"
+                        "profile")
+    # print(data.json())
+    # try:
+    ranks = data.json()["ratings"]
+    for rank in ranks:
+        pc[rank['role']] = rank['level']
+except Exception as e:
+    s.iprint(e, 1)
 
-data = requests.get("https://ow-api.com/v1/stats/xbl/Starkiller625/profile")
-# print(data.json())
-# try:
-xranks = data.json()["ratings"]
 xbox = {'tank': 0, 'damage': 0, 'support': 0}
-ranks = data.json()["ratings"]
-for rank in xranks:
-    xbox[rank['role']] = rank['level']
 
+try:
+    data = requests.get("https://ow-api.com/v1/stats/xbl/Starkiller625/profile")
+    # print(data.json())
+    # try:
+    xranks = data.json()["ratings"]
+    ranks = data.json()["ratings"]
+    for rank in xranks:
+        xbox[rank['role']] = rank['level']
+except Exception as e:
+    s.iprint(e, 1)
 
-rank_path = os.path.join(os.path.dirname(
-        os.path.abspath(__file__)), "ow_rolelock_ranks.txt")
+rank_path = (Path(__file__).parent / "ow_rolelock_ranks.txt").absolute()
 # print(rank_path)
+
+if not rank_path.exists():
+    rank_path.touch()
+
 with open(rank_path, "r") as fil:
     lines = fil.readlines()
     if str(now.date()) not in lines[-1]:
         need_rank = True
     else:
         need_rank = False
+
 if need_rank:
     with open(rank_path, "a") as fil:
         print("    Writing new SR")
